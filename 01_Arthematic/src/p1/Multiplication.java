8f757020623e0e28a7d8afef0c4f2b50d745f967
Md Shahnawaz Ahmad <102315055+Shahnawazahm@users.noreply.github.com>
2023-07-11 22:44:40 +0530

    uploaded

---
+++ 01_Arthematic/src/p1/Multiplication.java
@@ -0,0 +1,21 @@
+package p1;
+
+public class Multiplication {
+	public int n1; int n2;
+		
+	public void setN1(int n1) {
+		this.n1 = n1;
+	}
+	public void setN2(int n2) {
+		this.n2 = n2;
+	}
+	public int getN1() {
+		return n1;
+	}
+	public int getN2() {
+		return n2;
+	}
+	public int multiplication(int n1, int n2) {
+		return (n1*n2);
+	}
+}
